e9c512fc2c146f11f1d68cf06d3644ebbd6153ee
Kev <iuhfyi25282135@gmail.com>
2022-11-29 01:46:56 +0800

    update --hello world

--- DEMO/myapp/views.py
+++ DEMO/myapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def sayhello(request):
-    return render(request,'hello.html')
+    return render(request,'hello.html',{'name': 'Kev'})
 
