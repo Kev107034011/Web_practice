935cc8698d69f34c0e49d326dedeb36dfe045f8f
Kev <iuhfyi25282135@gmail.com>
2022-12-08 02:56:11 +0800

    update --hello world

--- DEMO/myapp/urls.py
+++ DEMO/myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 # URLConf
 urlpatterns = [
     path('hello/<str:username>',views.sayhello),
+    path('Show_img/<str:username>',views.showimg),
 ]
--- DEMO/myapp/views.py
+++ DEMO/myapp/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 from datetime import datetime
 # Create your views here.
 
-def sayhello(request):
+def sayhello(request,username):
     now = datetime.now()
     return render(request,'hello.html',locals()) #locals 包含 now 和 username 變數
 
+def showimg(request,username):
+    now = datetime.now()
+    return render(request,'Show_img.html',locals()) #locals 包含 now 和 username 變數
