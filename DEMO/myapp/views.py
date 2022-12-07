17a7204a2775b2c11de80bf7738604fd0b2e682b
Kev <iuhfyi25282135@gmail.com>
2022-12-07 19:21:08 +0800

    update --hello world

--- DEMO/myapp/views.py
+++ DEMO/myapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+def calculate():
+    x=1
+    y=2
+    return x+y
+
+
 def sayhello(request):
     x=1
     y=2
